449393375f39efcfea579fd535250da4b36af66c
Shinichiro Yamashita <ymst@ShinichironoMacBook-Air.local>
2020-07-05 14:48:58 +0900

    テストが通るよう実装

--- app/models/bank.rb
+++ app/models/bank.rb
@@ -1,5 +1,7 @@
 class Bank
   def reduce(source, to)
-    Money.dollar(10)
+    sum = source
+    amount = sum.augend.amount + sum.addend.amount
+    Money.new(amount: amount, currency: to)
   end
 end
