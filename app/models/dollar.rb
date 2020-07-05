70d1ba8de7d7ec20430e8197392d8bf06f3a2e98
Shinichiro Yamashita <ymst@ShinichironoMacBook-Air.local>
2020-07-05 11:24:47 +0900

    メソッドとして実装

--- app/models/dollar.rb
+++ app/models/dollar.rb
@@ -3,4 +3,8 @@ class Dollar < Money
   def times(multiplier)
     Dollar.new(amount: self.amount * multiplier)
   end
+
+  def currency
+    'USD'
+  end
 end
--- app/models/franc.rb
+++ app/models/franc.rb
@@ -3,4 +3,8 @@ class Franc < Money
   def times(multiplier)
     Franc.new(amount: self.amount * multiplier)
   end
+
+  def currency
+    'CHF'
+  end
 end
