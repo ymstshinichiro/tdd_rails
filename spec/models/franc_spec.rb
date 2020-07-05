1a07a5d148db8cfb4e2a82205b76e329d5f07a88
Shinichiro Yamashita <ymst@ShinichironoMacBook-Air.local>
2020-07-05 09:22:21 +0900

    テストを追加

--- spec/models/franc_spec.rb
+++ spec/models/franc_spec.rb
@@ -11,4 +11,13 @@ RSpec.describe Franc, type: :model do
     expect(ten.equals(five.times(2))).to be true
     expect(fifteen.equals(five.times(3))).to be true
   end
+
+  let(:another_five) { FactoryBot.create(:five_francs) }
+  let(:six) { FactoryBot.create(:six_francs) }
+
+  it '同一性、同値性の検証' do
+    # 別の角度から検証するテストを書く (三角測量)
+    expect(five.equals(another_five)).to be true
+    expect(five.equals(six)).to be false
+  end
 end
