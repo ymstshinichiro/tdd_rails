383369754f6a6c7e2b6cec7a5abdaad8df376eda
Shinichiro Yamashita <ymst@ShinichironoMacBook-Air.local>
2020-07-05 09:34:14 +0900

    ドルとフランを比較するテストを先に書く

--- spec/models/money_spec.rb
+++ spec/models/money_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Money, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:five_dollars) { FactoryBot.create(:five_dollars) }
+  let(:five_francs) { FactoryBot.create(:five_francs) }
+
+  it '同一性、同値性の検証' do
+    # 別の角度から検証するテストを書く (三角測量)
+    expect(five_dollars.equals(five_francs)).to be false
+  end
 end
