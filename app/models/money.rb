4853d48e70733b93efff1badcb966b3efc6acaeb
Shinichiro Yamashita <ymst@ShinichironoMacBook-Air.local>
2020-07-05 10:50:06 +0900

    Factoryを実装

--- app/models/money.rb
+++ app/models/money.rb
@@ -7,4 +7,8 @@ class Money < ApplicationRecord
   def equals(money)
     self.amount == money.amount && self.class == money.class
   end
+
+  def self.dollar(amount)
+    Dollar.new(amount: amount)
+  end
 end
