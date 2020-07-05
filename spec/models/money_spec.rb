1650451c9644925a84b9b545151de845f6911eed
Shinichiro Yamashita <ymst@ShinichironoMacBook-Air.local>
2020-07-05 11:13:21 +0900

    テストに通貨の概念を追加

--- spec/models/money_spec.rb
+++ spec/models/money_spec.rb
@@ -9,10 +9,16 @@ RSpec.describe Money, type: :model do
     expect(five_dollars.equals(five_francs)).to be false
   end
 
+  it '通貨の検証' do
+    # 別の角度から検証するテストを書く (三角測量)
+    expect(Money.dollar(1).currency).to eq 'USD'
+    expect(Money.franc(1).currency).to eq 'CHF'
+  end
+
   # オリジナルのテスト. timesの実装を強制させる
   let(:money) { FactoryBot.create(:money) }
 
-  it 'Moneyクラスのインスタンスはtimeを実装していなければならない' do
+  it 'Moneyクラスのインスタンスはtimesを実装していなければならない' do
     expect(money.respond_to?('times')).to be true
   end
 end
